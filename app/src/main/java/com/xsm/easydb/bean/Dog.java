35dca40fb1827ff34dd90bfa95e70a1e2fa775b6
xiashengming <xiasem@163.com>
2018-03-08 16:11:07 +0800

    增加bean类测试

---
+++ app/src/main/java/com/xsm/easydb/bean/Dog.java
@@ -0,0 +1,20 @@
+package com.xsm.easydb.bean;
+
+/**
+ * Author: 夏胜明
+ * Date: 2018/3/8 0008
+ * Email: xiasem@163.com
+ * Description:
+ */
+
+public class Dog {
+    private float weight;
+
+    public float getWeight() {
+        return weight;
+    }
+
+    public void setWeight(float weight) {
+        this.weight = weight;
+    }
+}
